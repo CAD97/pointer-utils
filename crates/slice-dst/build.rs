4cd12e351da5b157fe5e86c2b963a9104f66d73d
CAD97 <cad97@cad97.com>
2020-02-20 17:12:59 -0500

    yolo rc layout no longer known

--- crates/slice-dst/build.rs
+++ crates/slice-dst/build.rs
@@ -1,17 +1,9 @@
-use std::env;
-
 fn main() {
     let cfg = autocfg::new();
-    if let Ok(yolo) = env::var("YOLO_RC_LAYOUT_KNOWN") {
-        if !yolo.is_empty() && &yolo != "0" {
-            autocfg::emit("yolo_rc_layout_known")
-        }
-    }
     cfg.emit_expression_cfg(
         "std::ptr::slice_from_raw_parts_mut",
         "has_ptr_slice_from_raw_parts",
     );
 
-    autocfg::rerun_env("YOLO_RC_LAYOUT_KNOWN");
     autocfg::rerun_path("build.rs");
 }
