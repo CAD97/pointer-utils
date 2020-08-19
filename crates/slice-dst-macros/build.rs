56ec98e4fa44038c4c0d81f51ff0a32377c0a37d
CAD97 <cad97@cad97.com>
2020-08-19 11:49:58 -0500

    Nicer errors for slice-dst-macros buildscript
    that should help the user (and me) diagnose when building fails.
    I should probably add nicer error messages for some of the other
    likely failure points, but this will do for now and covers the main
    failure mode that I actually did encounter in CI.

--- crates/slice-dst-macros/build.rs
+++ crates/slice-dst-macros/build.rs
@@ -30,16 +30,67 @@ pub fn main() -> io::Result<()> {
 
     match child.wait() {
         Ok(status) if status.success() => match fs::copy(
-            impl_dir_path.join("target/wasm32-unknown-unknown/release/slice_dst_macros_impl.wasm"), 
-            impl_dir_path.join("../src/slice_dst_macros_impl.wasm")
+            impl_dir_path.join("target/wasm32-unknown-unknown/release/slice_dst_macros_impl.wasm"),
+            impl_dir_path.join("../src/slice_dst_macros_impl.wasm"),
         ) {
             Ok(_) => Ok(()),
             Err(e) => panic!("failed to copy wasm to final destination: {}", e),
         },
         Ok(_) => match fs::canonicalize("src/slice_dst_macros_impl.wasm") {
             Ok(_) => Ok(()), // succeed if the wasm is there already
-            Err(_) => panic!("failed to build wasm; you need the `wasm32-unknown-unknown` target to build slice-dst macros from source"),
+            Err(_) => error_could_not_build_wasm(impl_dir_path),
         },
         Err(e) => panic!("failed to wait for `cargo` subprocess (source: {})", e),
     }
+}
+
+fn error_could_not_build_wasm(impl_dir_path: &Path) -> ! {
+    let this_toolchain = match Command::new("rustup")
+        .args("show active-toolchain".split(' '))
+        .current_dir(impl_dir_path)
+        .output()
+    {
+        Ok(this_toolchain) => this_toolchain,
+        Err(_) => panic!(
+            "
+
+Could not build slice-dst macros, and you don't have `rustup` available.
+The command
+    build --release --target wasm32-unknown-unknown --target-dir target
+must succeed when run in the directory
+    {}
+to build slice-dst's macros from source. Either do what's required to make
+that command succeed in your installation of cargo, or duplicate a copy of
+`slice_dst_macros_impl.wasm` provided with distributions of this crate not
+from source (e.g. from crates-io) to the path
+    {}
+at which point building slice-dst's macros will work properly.
+
+",
+            impl_dir_path.display(),
+            impl_dir_path
+                .join("../src/slice_dst_macros_impl.wasm")
+                .display(),
+        ),
+    };
+
+    assert!(this_toolchain.status.success());
+    let this_toolchain = String::from_utf8_lossy(&this_toolchain.stdout);
+    panic!(
+        "
+
+Could not build slice-dst macros, likely because you don't have the
+`wasm32-unknown-unknown` target installed in your default profile.
+When building from distribution, the macro implementation wasm is provied
+precompiled, but when building from source, it's built using the default
+profile (and not an override for wherever the build command was issued).
+The toolchain being used to build the slice-dst macros is
+    {}
+and you should be able to fix this error by running
+    rustup target add wasm32-unknown-unknown --toolchain {}
+
+",
+        this_toolchain,
+        this_toolchain.split(' ').next().unwrap(),
+    );
 }
